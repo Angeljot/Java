c65989bfe6ffcca0ffd07c2fbec6ca0b01163e62
Angeljot <62397929+Angeljot@users.noreply.github.com>
2020-07-18 03:08:38 +0530

    Update If_Array_Is_Stack_Sortable.java

--- Stack Data Structure/If_Array_Is_Stack_Sortable.java
+++ Stack Data Structure/If_Array_Is_Stack_Sortable.java
@@ -1,4 +1,3 @@
-//Java Program to check whether array can be sorted using stack
 import java.util.*;
 
 class If_Array_Is_Stack_Sortable 
@@ -26,51 +25,12 @@ class If_Array_Is_Stack_Sortable
 			
 		
 			  
-            if (ar1[i - 1] > ar1[i]) 
-                return false; 
+            if (ar1[i - 1] > ar1[i])    //to check if previous element of an array is greater than current element
+                return false; //if it is so, then the array isn't sorted in an ascending order
+		// and hence it is not stack sortable
   
-         
-        return true; 
-		
-		
-		    
-		
-		
-	}
-	public static void main(String[] args)
-	{
-		import java.util.*;
-
-class Stack4 
-{
-	static boolean check(int ar[])
-	{
-		Stack<Integer> stack=new Stack<>();
-		int n=ar.length;
-		for(int i=0;i<=n-1;i++)
-		{
-			int c=ar[i];
-			stack.push(c);
-		}	
-		int[] ar1;
-		ar1 = new int[n];// allocating memory for 5 integers. 
-		for(int i=0;i<=n-1;i++)
-		{	
-		    	
-		    		ar1[i]=stack.pop();
-		    	
-		    	
-		    	
-        }
-		for(int i=1;i<=n-1;i++)
-			
-		
-			  // Unsorted pair found 
-            if (ar1[i - 1] > ar1[i]) 
-                return false; 
-  
-        // No unsorted pair found 
-        return true; 
+        
+        return true; //else it is a stack sortable
 		
 		
 		    
@@ -80,19 +40,17 @@ class Stack4
 	public static void main(String[] args)
 	{
 		Scanner s = new Scanner(System.in);
+              //use this method to get single line user input array in a Java
+	      System.out.println("Please enter numbers to store it in an array:");
+	      String numbers = s.nextLine(); //giving input of multiple numbers and storing them as a whole string
 
-	      System.out.println("Enter numbers you want to store in Array");
-	      String numbers = s.nextLine();//reading numbers and storing them as String datatype so to separate them afterwards and store it in array
-
-	      String ar[] = numbers.split(" "); //splitting string by space and storing it in string array
-          int n=ar.length;//finding length of "ar" array and storing it in "n" variable
-	      int a[] = new int[n]; //creating integer array 
+	      String ar[] = numbers.split(" "); //splitting whole string and storing them into string array
+          int n=ar.length;//finding length of string array
+	      int a[] = new int[n];//defining integer array where the numbers will be stored in integer datatype
 	      for (int i = 0; i < a.length; ++i) {
-	          a[i] = Integer.parseInt(ar[i]); //numbers stored in "ar" are being parsed into integer and then storing it in an integer array
+	          a[i] = Integer.parseInt(ar[i]);//parsing string into integer and storing numbers in integer datatype
 	      }
-		System.out.println(check(a));
+		System.out.println(check(a)); //calling function
 	}
 
 }
-
-	
