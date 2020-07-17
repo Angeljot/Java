030d00ae3d8898a659511b3c68a6b830b81e4f5c
Angeljot <62397929+Angeljot@users.noreply.github.com>
2020-07-18 02:42:09 +0530

    Add files via upload

---
+++ Stack Data Structure/Stack2.java
@@ -0,0 +1,53 @@
+//Program to convert prefix to infix conversion
+import java.util.Stack;
+import java.util.Scanner;
+class Stack2
+{
+	static String convert(String exp)
+	{
+		String result=new String("");
+		String temp=new String("");
+		Stack<String> stack=new Stack<>();
+		for(int i=exp.length()-1;i>=0;i--) 
+		{
+			char c=exp.charAt(i);
+			if(isOperator(c))
+			{
+				String s1=stack.pop();
+				String s2=stack.pop();
+				temp='('+s1+c+s2+')';
+				stack.push(temp);
+				
+			}
+			else
+			{
+				stack.push(c+"");
+				
+			}
+		}
+		result=stack.pop();
+		return result;
+				
+	}
+	static boolean isOperator(char ch)
+	{
+		switch(ch)
+		{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '^':
+			return true;
+		}
+		return false;
+		
+	}
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		String exp=s.nextLine();
+		System.out.println(convert(exp));
+		
+	}
+}
